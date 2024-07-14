a33ab165c0b851d53ed940536aeb0b2a65dc2ef2
ferdouszihad <f.zihad333@gmail.com>
2024-07-14 16:47:26 +0600

    mvc architecture build

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const chalk = require("chalk");
 dotenv.config();
+mongoose.set("strictQuery", true);
 const port = process.env.PORT || 5000;
 const app = require("./app");
 
-// mongoose
-//   .connect("mongodb://localhost:27017/ecommerce")
-//   .then(() => console.log("connected"))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log(chalk.bgBlue.bold(`Database Connected `)))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
-  console.log(chalk.bgRed(`server is running on port ${port}`));
+  console.log(chalk.bgRed.bold(`LMS server is running \nport ${port} `));
 });
